8131f5c2361f667b6921f50dc03eb3df7ddc7a78
Michael Jordan <mjordan@quiverware.com>
2024-04-09 21:36:49 -0600

    Add image

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Quiverware LLC',
+      title: 'Quiverware',
       home: Scaffold(
         body: HomeContentDesktop(),
         // body: Center(
@@ -30,33 +30,52 @@ class MainApp extends StatelessWidget {
 class HomeContentDesktop extends StatelessWidget {
   const HomeContentDesktop({super.key});
 
+  static const _imageReference =
+      'assets/images/futuristic_digital_interface_.jpg';
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: <Widget>[
-        // const WelcomeHeader(),
-        ConstrainedBox(
-          constraints: const BoxConstraints(maxWidth: 1250),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Container(
-                color: Colors.blue[600],
-                child: const Center(
-                  child: Text(
-                    'Hello World!!',
-                    style: TextStyle(
-                      fontSize: 48,
+    return Stack(
+      children: [
+        Image.asset(
+          _imageReference,
+          scale: 1.0, //1.6,
+        ),
+        Container(
+          color: Colors.black45,
+        ),
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            const Text(
+              'Quiverware',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 48,
+              ),
+            ),
+            ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: 1250),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    color: Colors.blue[600],
+                    child: const Center(
+                      child: Text(
+                        'Hello World!!',
+                        style: TextStyle(
+                          fontSize: 48,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              )
-              // EventDetails(),
-            ],
-          ),
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
       ],
     );
